000947b39b26f6f3643b945b665a79beb617e7a1
Nikita <mynewacc.create@gmail.com>
2020-03-18 18:31:49 +0300

    implement class User

--- Network Programming/src/bsu/rct/group8/chernyak/lab7/var1c/User.java
+++ Network Programming/src/bsu/rct/group8/chernyak/lab7/var1c/User.java
@@ -1,5 +1,24 @@
 package bsu.rct.group8.chernyak.lab7.var1c;
 
 public class User {
-
+	
+	private String name;
+	private String addres;
+	
+	public User(String name, String addres) {
+		this.name = name;
+		this.addres = addres;
+	}
+	
+	public void displayUser(){
+		System.out.println(name + " " + addres);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getAddres() {
+		return addres;
+	}
 }
